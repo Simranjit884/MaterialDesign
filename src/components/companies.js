ae1f45d13e288d4fa4aa6f92c72f8332667546fc
samy884 <ssgill884@gmail.com>
2018-11-12 20:50:42 +0530

    modified

--- src/components/companies.js
+++ src/components/companies.js
@@ -1,19 +1,24 @@
 import React,{Component} from 'react';
 import {connect} from 'react-redux';
 import {getlist} from '../actions';
-
+//import CompaniesBalance from '../data1.json';
 
 class Companies extends Component{
     componentDidMount(){
         this.props.getlist();
     }
     render(){
-        if(!this.props.company) return <div>Loading...</div>
+        if(this.props.company[0]===undefined){ 
+            return <div>Loading...</div>
+        }
         const {id}=this.props.match.params;
         let name=this.props.company.filter(element=>element.id===id);
+        console.log(name[0].aliasName);
+        //const bal=CompaniesBalance.filter(element=>element.id===id);
         return (
             <div>
-                id is={id}         
+                <p>id is={id}</p> 
+                <p>name is={name[0].aliasName}</p>
             </div>
         );
     }   
